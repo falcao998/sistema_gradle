9ced5e8da3be32f843bb2cb79381839e91a3b3d1
Felipe Souza Cerqueira dos Santos <felipesouza_analista@yahoo.com.br>
2020-05-28 18:35:10 -0300

    Commit Spring-Boot + Gradle

---
+++ src/main/java/br/com/sys/souza/sistema_gradle/SistemaGradleApplication.java
@@ -0,0 +1,13 @@
+package br.com.sys.souza.sistema_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGradleApplication.class, args);
+	}
+
+}
